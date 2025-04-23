741693601fe25c169bb367b39fb8f6d83cbb10f6
albertsimonyan74 <albert.simonyan.w@gmail.com>
2025-04-23 23:58:45 +0400

    Changing the visualization function

--- utils/audio_processing.py
+++ utils/audio_processing.py
@@ -25,18 +25,24 @@ def extract_mfcc_from_file(file_path, sample_rate=16000, num_mfcc=13, max_len=3)
         print(f"⚠️ Error with {file_path}: {e}")
         return None
 
-def visualize_waveform_and_mfcc(file_path, sr=16000, max_len=3):
+def visualize_waveform_and_mel(file_path, sr=16000, max_len=3):
     y, sr = load_audio(file_path, sr=sr, max_len=max_len)
-    mfcc = extract_mfcc(y, sr)
+    
+    mel_spec = librosa.feature.melspectrogram(y=y, sr=sr)
+    mel_db = librosa.power_to_db(mel_spec, ref=np.max)
 
-    plt.figure(figsize=(12, 4))
+    print(f"Signal shape: {y.shape}, Mel shape: {mel_db.shape}")
+
+    plt.figure(figsize=(14, 5))
+
     plt.subplot(1, 2, 1)
     plt.title("Waveform")
     librosa.display.waveshow(y, sr=sr)
 
     plt.subplot(1, 2, 2)
-    plt.title("MFCC")
-    librosa.display.specshow(mfcc, x_axis='time', sr=sr)
-    plt.colorbar()
+    plt.title("Mel Spectrogram")
+    librosa.display.specshow(mel_db, x_axis='time', y_axis='mel', sr=sr, fmax=8000)
+    plt.colorbar(format='%+2.0f dB')
+
     plt.tight_layout()
     plt.show()
